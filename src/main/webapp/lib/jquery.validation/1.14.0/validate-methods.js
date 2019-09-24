fc2fe5bfbfa2731266dd3accc17eb4010f6fda8e
w2481296 <wm506575989@qq.com>
2019-09-24 18:50:47 +0800

    fix#修改

--- src/main/webapp/lib/jquery.validation/1.14.0/validate-methods.js
+++ src/main/webapp/lib/jquery.validation/1.14.0/validate-methods.js
@@ -196,39 +196,3 @@ $(function(){
 		return this.optional(element) || (tel.test(value)); 
 	},"请输入正确车牌号");
 });
-//身份证号码的验证规则
-function isIdCardNo(num){ 
-　   //if (isNaN(num)) {alert("输入的不是数字！"); return false;} 
-　　 var len = num.length, re; 
-　　 if (len == 15) 
-　　 re = new RegExp(/^(\d{6})()?(\d{2})(\d{2})(\d{2})(\d{2})(\w)$/); 
-　　 else if (len == 18) 
-　　 re = new RegExp(/^(\d{6})()?(\d{4})(\d{2})(\d{2})(\d{3})(\w)$/); 
-　　 else {
-		//alert("输入的数字位数不对。"); 
-		return false;
-	} 
-　　 var a = num.match(re); 
-　　 if (a != null) 
-　　 { 
-　　 if (len==15) 
-　　 { 
-　　 var D = new Date("19"+a[3]+"/"+a[4]+"/"+a[5]); 
-　　 var B = D.getYear()==a[3]&&(D.getMonth()+1)==a[4]&&D.getDate()==a[5]; 
-　　 } 
-　　 else 
-　　 { 
-　　 var D = new Date(a[3]+"/"+a[4]+"/"+a[5]); 
-　　 var B = D.getFullYear()==a[3]&&(D.getMonth()+1)==a[4]&&D.getDate()==a[5]; 
-　　 } 
-　　 if (!B) {
-		//alert("输入的身份证号 "+ a[0] +" 里出生日期不对。"); 
-		return false;
-	} 
-　　 } 
-　　 if(!re.test(num)){
-		//alert("身份证最后一位只能是数字和字母。");
-		return false;
-	}
-　　 return true; 
-}
