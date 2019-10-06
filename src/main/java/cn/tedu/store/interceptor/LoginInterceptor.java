75f25c0ee85f89ff4acba43595317fa84e5bd83b
w2481296 <wm506575989@qq.com>
2019-10-06 19:39:11 +0800

    fix#

--- src/main/java/cn/tedu/store/interceptor/LoginInterceptor.java
+++ src/main/java/cn/tedu/store/interceptor/LoginInterceptor.java
@@ -10,15 +10,8 @@ import org.springframework.web.servlet.ModelAndView;
 import cn.tedu.store.bean.User;
 
 public class LoginInterceptor implements HandlerInterceptor{
-	//ִ�п���������֮ǰִ��
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		// TODO Auto-generated method stub
-		//1.��ȡ��session����
-		//��session��ȡ��user
-		//2.Object obj==null;˵��û��¼��
-		//��ô��ת����¼ҳ�档�ض��򷵻�false
-		//3.����return true;
 		HttpSession session=request.getSession();
 		User user=(User) session.getAttribute("user");
 		if(user==null){
@@ -28,16 +21,12 @@ public class LoginInterceptor implements HandlerInterceptor{
 		}
 		return true;
 	}
-	//���Ʒ���ִ��֮�󣬵�����Ӧҳ��֮ǰ
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
-		// TODO Auto-generated method stub
 		
 	}
-	//��Ӧҳ��֮��
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
-		// TODO Auto-generated method stub
 		
 	}
 	
